5644413bc3ec512007d623a87830d0d9591401c4
Fahim Ahmed <fahim.ahmed05@outlook.com>
2024-11-23 22:34:38 +0600

    changed mullvad dns to adguard dns

--- Firefox/user.js
+++ Firefox/user.js
@@ -34,10 +34,10 @@ user_pref("network.http.pacing.requests.enabled", false); // Disable HTTP reques
 user_pref("network.dnsCacheExpiration", 3600); // DNS cache expiration time
 user_pref("network.ssl_tokens_cache_capacity", 10240); // SSL tokens cache capacity
 user_pref("network.trr.mode", 3); // Use DNS over HTTPS (DoH) as primary resolver
-user_pref("network.trr.uri", "https://base.dns.mullvad.net/dns-query"); // Mullvad DoH URI
-user_pref("network.trr.custom_uri", "https://base.dns.mullvad.net/dns-query"); // Custom Mullvad DoH URI
+user_pref("network.trr.uri", "https://dns.adguard-dns.com/dns-query"); // DoH URI
+user_pref("network.trr.custom_uri", "https://dns.adguard-dns.com/dns-query"); // Custom DoH URI
 user_pref("network.dns.disableIPv6", true); // Disable IPv6 for DNS resolution
-user_pref("network.trr.bootstrapAddress", "194.242.2.4"); // Mullvad IP for DoH bootstrap
+user_pref("network.trr.bootstrapAddress", "94.140.14.14"); // Adguard IP for DoH bootstrap
 
 // =============================================
 // Prefetching Settings
