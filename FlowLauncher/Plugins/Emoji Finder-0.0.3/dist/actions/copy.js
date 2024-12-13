92d1ddcd00a0db638450c1e0f51bb5159a4dd36b
Fahim Ahmed <fahim.ahmed05@outlook.com>
2024-12-13 06:29:08 +0600

    update

--- FlowLauncher/Plugins/Emoji Finder-0.0.3/dist/actions/copy.js
+++
@@ -1,9 +0,0 @@
-import clipboard from 'clipboardy';
-import { FlowLauncher } from '../utils/flowLauncher.js';
-export function copy(text) {
-    if (text !== undefined) {
-        clipboard.writeSync(text);
-        FlowLauncher.showMessage(text, 'Copied to clipboard!');
-    }
-}
-//# sourceMappingURL=copy.js.map
--- FlowLauncher/Plugins/Emoji Finder-0.0.3/dist/actions/query.js
+++
@@ -1,7 +0,0 @@
-import { find } from '../utils/find.js';
-import { sendJsonRpcRequest } from '../utils/flowLauncher.js';
-export function query(input) {
-    const result = find(input ?? '');
-    sendJsonRpcRequest({ result });
-}
-//# sourceMappingURL=query.js.map
