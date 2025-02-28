b0914f46a69349c72a3d678eee132bea166f5a8a
Fahim Ahmed <fahim.ahmed05@outlook.com>
2025-03-01 00:36:42 +0600

    add Windows Sandbox startup script for dark mode, wallpaper, and context menu enhancements

---
+++ WindowsSandbox/SandboxStartup.ps1
@@ -0,0 +1,52 @@
+# Enable Dark Mode for Apps
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
+
+# Enable Dark Mode for System
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
+
+# Set the Wallpaper
+$wallpaperPath = "C:\Windows\Web\Wallpaper\Windows\img19.jpg"
+$code = @'
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+'@
+
+Add-Type $code
+$SPI_SETDESKWALLPAPER = 0x0014
+$UPDATE_INI_FILE = 0x01
+$SEND_CHANGE = 0x02
+
+[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $wallpaperPath, ($UPDATE_INI_FILE -bor $SEND_CHANGE))
+
+# Change context menu to old style
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_ShowClassicMode" /t REG_DWORD /d 1 /f
+
+# Show file extensions
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d 0 /f
+
+# Show hidden files
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d 1 /f
+
+# Add 'Open PowerShell Here' and 'Open CMD Here' to context menu
+$powershellPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+$cmdPath = "C:\Windows\System32\cmd.exe"
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyPowerShell" /ve /d "Open PowerShell Here" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyPowerShell" /v "Icon" /t REG_SZ /d "$powershellPath,0" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyPowerShell\command" /ve /d "powershell.exe -noexit -command Set-Location -literalPath '%V'" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Mycmd" /ve /d "Open CMD Here" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Mycmd" /v "Icon" /t REG_SZ /d "$cmdPath,0" /f
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Mycmd\command" /ve /d "cmd.exe /s /k cd /d `"\`"%V`"\`"" /f
+
+# Restart Explorer so changes take effect
+Stop-Process -Name explorer -Force
+
+# Open an explorer window to the host-shared folder
+Start-Process explorer.exe C:\Users\WDAGUtilityAccount\Desktop\WinsbSharedFolder
+
+# Change execution policy for powershell to allow running scripts
+Set-ExecutionPolicy -ExecutionPolicy unrestricted -Scope LocalMachine
+
