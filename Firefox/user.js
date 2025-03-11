143042d85643b26a6bf2b4cc98214a06b29457a8
Fahim Ahmed <fahim.ahmed05@outlook.com>
2025-03-12 00:53:48 +0600

    update Firefox user.js with enhanced preferences for performance, caching, telemetry, and UI adjustments

--- Firefox/user.js
+++ Firefox/user.js
@@ -1,16 +1,50 @@
-// DNS Over Https
+// GENERAL
+user_pref("content.notify.interval", 100000);
+
+// GFX
+user_pref("gfx.canvas.accelerated.cache-size", 512);
+user_pref("gfx.content.skia-font-cache-size", 20);
+
+// DISK CACHE
+user_pref("browser.cache.disk.enable", false);
+
+// MEDIA CACHE 
+user_pref("media.memory_cache_max_size", 65536);
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("media.cache_resume_threshold", 3600);
+
+// IMAGE CACHE 
+user_pref("image.mem.decode_bytes_at_a_time", 32768);
+
+// NETWORK 
+user_pref("network.http.max-connections", 1800);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
+user_pref("network.dns.disableIPv6", true);
+
+// SPECULATIVE LOADING 
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("network.prefetch-next", false);
+user_pref("network.predictor.enabled", false);
+user_pref("network.predictor.enable-prefetch", false);
+
+// EXPERIMENTAL 
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+
+// DNS OVER HTTPS
 user_pref("network.trr.mode", 3);
 user_pref("network.trr.uri", "https://dns.adguard-dns.com/dns-query");
 user_pref("network.trr.custom_uri", "https://dns.adguard-dns.com/dns-query");
 user_pref("network.trr.bootstrapAddress", "94.140.14.14");
 
-// IPv6
-user_pref("network.dns.disableIPv6", true);
-
-// Bookmark Bar
+// BOOKMARKS BAR
 user_pref("browser.tabs.loadBookmarksInBackground", true);
 
-// Smooth Scrolling
+// SMOOTH SCROLLING
 user_pref("apz.overscroll.enabled", true);
 user_pref("general.smoothScroll", true);
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
@@ -24,9 +58,62 @@ user_pref("general.smoothScroll.currentVelocityWeighting", "1");
 user_pref("general.smoothScroll.stopDecelerationWeighting", "1");
 user_pref("mousewheel.default.delta_multiplier_y", 300);
 
-// Website Permission
+// WEBSITE PERMISSIONS
+user_pref("permissions.default.xr", 2);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.geo", 2);
 
-// Enable Tab Hover Preview
+// TAB PREVIEW ON HOVER
 user_pref(browser.tabs.hoverPreview.enabled, true);
+
+// TELEMETRY 
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+
+// EXPERIMENTS 
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+
+// CRASH REPORTS 
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+
+// DETECTION 
+user_pref("captivedetect.canonicalURL", "");
+user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.enabled", false);
+
+// MOZILLA UI 
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false)
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.aboutwelcome.enabled", false);
+
+// THEME ADJUSTMENTS 
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// FULLSCREEN NOTICE 
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.timeout", 0);
+
+// URL BAR 
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
