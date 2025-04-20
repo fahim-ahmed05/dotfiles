81c7e077f4d624e89f6427ddfb582a7ebc4dbebd
Fahim Ahmed <fahim.ahmed05@outlook.com>
2025-04-20 13:30:34 +0600

    feat: add su function for elevated command execution in Windows Terminal

--- ShellScripts/Microsoft.PowerShell_profile.ps1
+++ ShellScripts/Microsoft.PowerShell_profile.ps1
@@ -259,3 +259,15 @@ function gsudo {
         Start-Process "wt.exe" -ArgumentList @("-d $currentDir", "-p PowerShell", "pwsh -NoExit -Command $Command") -Verb RunAs
     }
 }
+
+# SU
+function su {
+    $currentDir = Get-Location
+    if ($args.Count -gt 0) {
+        $argList = $args -join ' '
+        Start-Process wt -Verb runAs -ArgumentList "-d $currentDir -p PowerShell pwsh -NoExit -Command $argList"
+    }
+    else {
+        Start-Process wt -Verb runAs -ArgumentList "-d $currentDir"
+    }
+}
