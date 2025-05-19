fb479cd3885e0f201366d95db0103b2a44040489
Fahim Ahmed <fahim.ahmed05@outlook.com>
2025-05-19 16:14:27 +0600

    chore: add user preferences for tab behavior, window controls, website permissions, and network settings

---
+++ Zen/user.js
@@ -0,0 +1,31 @@
+// TAB BEHAVIOR
+user_pref("browser.tabs.loadBookmarksInBackground", true);
+user_pref("browser.tabs.hoverPreview.enabled", true);
+
+// WINDOW CONTROLS
+user_pref("zen.view.experimental-no-window-controls", true);
+
+// WEBSITE PERMISSIONS
+user_pref("permissions.default.xr", 2);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.camera", 2);
+user_pref("permissions.default.microphone", 2);
+user_pref("permissions.manager.defaultsUrl", "");
+user_pref("media.autoplay.default", 5);
+
+// DOH & IPV6
+user_pref("network.trr.mode", 3);
+user_pref("network.trr.uri", "https://dns.adguard-dns.com/dns-query");
+user_pref("network.trr.custom_uri", "https://dns.adguard-dns.com/dns-query");
+user_pref("network.trr.bootstrapAddress", "94.140.14.14");
+user_pref("network.dns.disableIPv6", true);
+
+// BROWSING HISTORY
+user_pref("privacy.history.custom", true);
+user_pref("places.history.enabled", true);
+user_pref("browser.formfill.enable", false);
+
+// PICTURE-IN-PICTURE
+user_pref("media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled", true);
+
