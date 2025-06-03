171132c3c5b6c2b0bf6506c38c05b2cff77f451f
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2025-06-03 10:51:01 +0600

    chore: remove experimental window controls and unused preferences from user.js

--- Zen/user.js
+++ Zen/user.js
@@ -2,24 +2,18 @@
 user_pref("browser.tabs.loadBookmarksInBackground", true);
 user_pref("browser.tabs.hoverPreview.enabled", true);
 
-// WINDOW CONTROLS
-user_pref("zen.view.experimental-no-window-controls", true);
-
 // WEBSITE PERMISSIONS
 user_pref("permissions.default.xr", 2);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.geo", 2);
 user_pref("permissions.default.camera", 2);
 user_pref("permissions.default.microphone", 2);
-user_pref("permissions.manager.defaultsUrl", "");
-user_pref("media.autoplay.default", 5);
 
 // DOH & IPV6
 user_pref("network.trr.mode", 3);
 user_pref("network.trr.uri", "https://dns.adguard-dns.com/dns-query");
 user_pref("network.trr.custom_uri", "https://dns.adguard-dns.com/dns-query");
 user_pref("network.trr.bootstrapAddress", "94.140.14.14");
-user_pref("network.dns.disableIPv6", true);
 
 // BROWSING HISTORY
 user_pref("privacy.history.custom", true);
@@ -28,9 +22,6 @@ user_pref("browser.formfill.enable", false);
 
 // PICTURE-IN-PICTURE
 user_pref("media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled", true);
-
-// POCKET
-user_pref("extensions.pocket.enabled", true);
 
 // ZEN UI
 user_pref("browser.shell.checkDefaultBrowser", false);
