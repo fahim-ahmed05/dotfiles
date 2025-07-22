deb5e5918163c4f37b94e4c0363dcd3221b9d4fe
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2025-07-22 10:17:03 +0600

    add: enable spicetify update in pipx upgrade function

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -151,8 +151,8 @@ function wu {
     Write-Host "`n📦  Updating pipx packages...`n" -ForegroundColor Cyan
     pipx upgrade-all
 
-#    Write-Host "`n📦  Updating spicetify...`n" -ForegroundColor Cyan
-#    spicetify update
+    Write-Host "`n📦  Updating spicetify...`n" -ForegroundColor Cyan
+    spicetify update
 
     rmDesktopIcons
 }
