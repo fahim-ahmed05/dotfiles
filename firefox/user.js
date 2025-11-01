29b7ca5e000463dac6370efd6a6396283cd6432c
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2025-11-01 12:23:40 +0600

    fix: reorganize user.js for improved structure and performance settings

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,66 @@
 // =========================
+// GENERAL
+// =========================
+user_pref("gfx.content.skia-font-cache-size", 32);
+
+// =========================
+// GFX
+// =========================
+user_pref("gfx.canvas.accelerated.cache-items", 32768);
+user_pref("gfx.canvas.accelerated.cache-size", 4096);
+user_pref("webgl.max-size", 16384);
+
+// =========================
+// DISK CACHE
+// =========================
+user_pref("browser.cache.disk.enable", false);
+
+// =========================
+// MEMORY CACHE
+// =========================
+user_pref("browser.cache.memory.capacity", 131072);
+user_pref("browser.cache.memory.max_entry_size", 20480);
+user_pref("browser.sessionhistory.max_total_viewers", 4);
+user_pref("browser.sessionstore.max_tabs_undo", 10);
+
+// =========================
+// MEDIA CACHE
+// =========================
+user_pref("media.memory_cache_max_size", 262144);
+user_pref("media.memory_caches_combined_limit_kb", 1048576);
+user_pref("media.cache_readahead_limit", 600);
+user_pref("media.cache_resume_threshold", 300);
+
+// =========================
+// IMAGE CACHE
+// =========================
+user_pref("image.cache.size", 10485760);
+user_pref("image.mem.decode_bytes_at_a_time", 65536);
+
+// =========================
+// NETWORK
+// =========================
+user_pref("network.http.max-connections", 1800);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.request.max-start-delay", 5);
+user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.dnsCacheEntries", 10000);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
+
+// =========================
+// SPECULATIVE LOADING
+// =========================
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("browser.places.speculativeConnect.enabled", false);
+user_pref("network.prefetch-next", false);
+user_pref("network.predictor.enabled", false);
+
+// =========================
 // DNS-OVER-HTTPS & IPv6
 // =========================
 user_pref("network.trr.mode", 3);
