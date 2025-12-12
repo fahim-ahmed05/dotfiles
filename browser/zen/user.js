8111922f8e2a73a71226652a62521338335b0ef0
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2025-12-12 16:19:43 +0600

    refactor user.js for improved organization and clarity of preferences

--- browser/zen/user.js
+++ browser/zen/user.js
@@ -1,10 +1,8 @@
-// TAB BEHAVIOR
-user_pref("browser.tabs.loadBookmarksInBackground", true);
+// BROWSER BEHAVIOR & UI
 user_pref("browser.tabs.hoverPreview.enabled", true);
-user_pref("browser.tabs.closeWindowWithLastTab", false);
-user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+user_pref("browser.tabs.loadBookmarksInBackground", true);
 
-// WEBSITE PERMISSION
+// PERMISSIONS DEFAULTS
 user_pref("permissions.default.xr", 2);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.geo", 2);
@@ -13,43 +11,38 @@ user_pref("permissions.default.microphone", 2);
 user_pref("permissions.manager.defaultsUrl", "");
 user_pref("media.autoplay.default", 5);
 
-// DOH & IPV6
+// DNS-OVER-HTTPS & IPv6
 user_pref("network.trr.mode", 3);
 user_pref("network.trr.uri", "https://dns.adguard-dns.com/dns-query");
 user_pref("network.trr.custom_uri", "https://dns.adguard-dns.com/dns-query");
 user_pref("network.trr.bootstrapAddress", "94.140.14.14");
 user_pref("network.dns.disableIPv6", true);
 
-// TRACKING PROTECTION
+// HTTPS-ONLY MODE
+user_pref("dom.security.https_only_mode", true);
+
+// ENHANCED PRIVACY FEATURES
 user_pref("browser.contentblocking.category", "custom");
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("network.cookie.cookieBehavior", 1);
 user_pref("privacy.trackingprotection.cryptomining.enabled", true);
+user_pref("privacy.fingerprintingProtection", true);
 user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
-user_pref("privacy.trackingprotection.fingerprinting.probable.enabled", true);
 user_pref("privacy.trackingprotection.allow_list.baseline.enabled", false);
 user_pref("privacy.trackingprotection.allow_list.convenience.enabled", false);
-user_pref("privacy.trackingprotection.socialtracking.enabled", true);
-user_pref("privacy.trackingprotection.antifraud.skip.enabled", true);
-user_pref("privacy.trackingprotection.consentmanager.skip.enabled", true);
-user_pref("privacy.trackingprotection.emailtracking.enabled", true);
-user_pref("privacy.antitracking.isolateContentScriptResources", true);
-
-// HTTPS-ONLY MODE
-user_pref("dom.security.https_only_mode", true);
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("browser.privatebrowsing.resetPBM.enabled", true);
+user_pref("security.mixed_content.block_display_content", true);
+user_pref("network.auth.subresource-http-auth-allow", 1);
 
 // BROWSING HISTORY
+user_pref("browser.formfill.enable", false);
 user_pref("privacy.history.custom", true);
 user_pref("places.history.enabled", true);
-user_pref("browser.formfill.enable", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
 
 // PICTURE-IN-PICTURE
 user_pref("media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled", true);
-
-// ZEN
-user_pref("zen.watermark.enabled", false)
-user_pref("zen.view.experimental-no-window-controls", true);
 
 // UI
 user_pref("browser.shell.checkDefaultBrowser", false);
@@ -58,23 +51,14 @@ user_pref("browser.aboutwelcome.enabled", false);
 user_pref("startup.homepage_welcome_url", "");
 user_pref("startup.homepage_welcome_url.additional", "");
 user_pref("browser.aboutConfig.showWarning", false);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// MOZILLA
-user_pref("signon.firefoxRelay.feature", "disabled");
-user_pref("browser.firefox-view.feature-tour", '{"screen":"","complete":true}');
-
-// SEARCH
-user_pref("browser.search.suggest.enabled", true);
-user_pref("browser.search.suggest.enabled.private", true);
-
-// DOWNLOAD
+// DOWNLOADS
+user_pref("browser.download.manager.addToRecentDocs", false);
 user_pref("browser.download.alwaysOpenPanel", false);
-user_pref("browser.download.autohideButton", true);
 user_pref("browser.download.always_ask_before_handling_new_types", false);
 user_pref("browser.download.start_downloads_in_tmp_dir", true);
 
-// PDF
-user_pref("pdfjs.enableScripting", false);
+// PDF VIEWER
 user_pref("browser.download.open_pdf_attachments_inline", false);
+user_pref("pdfjs.enableScripting", false);
 
