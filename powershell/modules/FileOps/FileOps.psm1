ee3a1be2a487f22021fcb3a68d47f562fb679496
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2026-05-16 12:45:03 +0600

    add: Add-RemoveRegFiles function

--- powershell/modules/FileOps/FileOps.psm1
+++ powershell/modules/FileOps/FileOps.psm1
@@ -24,6 +24,10 @@ function Clear-Folder {
     & "$env:UserProfile\Git\dotfiles\powershell\scripts\Clear-Folder.ps1" -ConfigPath "$env:UserProfile\Git\dotfiles\powershell\configs\clear_folders_$computer.json" @args
 }
 
+function Add-RemoveRegFiles {
+    & "$env:UserProfile\Git\dotfiles\powershell\scripts\Add-RemoveRegFiles.ps1" -Config "$env:UserProfile\Git\dotfiles\powershell\configs\reg_files.json" @args
+}
+
 function Remove-DesktopIcons { Clear-Folder "$env:UserProfile\Desktop\*.lnk" "$env:PUBLIC\Desktop\*.lnk" }
 
 
