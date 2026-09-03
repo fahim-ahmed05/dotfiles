9b8e6f1b09cc921fd333b72c6fb235d0539c4760
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2026-09-03 18:44:44 +0600

    feat: remove 'grep' alias from PowerShell profile

--- shell/pwsh/Microsoft.PowerShell_profile.ps1
+++ shell/pwsh/Microsoft.PowerShell_profile.ps1
@@ -46,7 +46,6 @@ function curl {
 Set-Alias -Name ls -Value eza
 Set-Alias -Name ff -Value fzf
 Set-Alias -Name cd -Value z -Option AllScope
-Set-Alias -Name grep -Value rg
 
 # Prompt
 oh-my-posh init pwsh --config 'robbyrussell' | Invoke-Expression
